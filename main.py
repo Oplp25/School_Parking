467383628b5d40495680474573caa4aa08f0d048
Alfred Sorrell <alfred.sorrell@gmail.com>
2022-11-30 10:16:00 +0000

    capitalize first letter

--- main.py
+++ main.py
@@ -1,1 +1,1 @@
-#this will be the main file, where we call all the functions and this will be what we run when the code is completed.
+#This will be the main file, where we call all the functions and this will be what we run when the code is completed.
