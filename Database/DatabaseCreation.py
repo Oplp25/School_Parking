dee9143563bc8e3b6b3dc64c44dde910a51b3d22
Alfred Sorrell <alfred.sorrell@gmail.com>
2022-11-30 10:58:11 +0000

    Database Framework

--- Database/DatabaseCreation.py
+++ Database/DatabaseCreation.py
@@ -21,4 +21,28 @@ class DB:
                 """)
         self.conn.commit()
 
+    def open(self):
+        self.conn = sqlite3.connect("Database/Users.db", detect_types=sqlite3.PARSE_DECLTYPES |
+                                    sqlite3.PARSE_COLNAMES)
+        self.cur = self.conn.cursor()
+
+    def close(self):
+        self.conn.close()
+
+    def addUser(self, rqName, rqHasSpace, rqIsStaff, rqHasPass, rqInfractionsCount, rqDatePassStarted="", rqDatePassEnds=""):
+        self.open()
+        self.cur.execute("INSERT INTO users (name, hasSpace, isStaff, hasPass, infractionsCount, datePassStarted, datePassEnds) VALUES (?,?,?,?,?,?,?)",
+                         (rqName, rqHasSpace, rqIsStaff, rqHasPass, rqInfractionsCount, rqDatePassStarted, rqDatePassEnds))
+        self.conn.commit()
+        self.close()
+    
+    def removeUser(self, rqUserID):
+        self.open()
+        self.cur.execute("DELETE FROM users WHERE userID = ?",(rqUserID,))
+        self.conn.commit()
+        self.close()
+
+
 userDB = DB()
+
+userDB.removeUser(1)
