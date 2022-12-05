8b5daf8c101fa2985b2477f4f1e8f9d1b0d7d18f
Alfred Sorrell <alfred.sorrell@gmail.com>
2022-12-05 14:43:25 +0000

    database done

--- Website/Registration/registrationCode.py
+++ Website/Registration/registrationCode.py
@@ -2,23 +2,26 @@ import cherrypy
 import os, sys
 sys.path.insert(0,os.getcwd()+r"\Database\databaseCode")
 import loginDatabase
+import usersDatabase
 
 lDB = loginDatabase.LoginDB()
+uDB = usersDatabase.DB()
 
 class RegistrationWebpage(object):
     @cherrypy.expose
     def index(self):
-        return open(os.path.abspath(__file__)[:-11]+"\clientRegistration.html")
+        return open(os.getcwd()+r"\Website\Registration\clientRegistration.html")
 
     @cherrypy.expose
-    def registerUser(self, user=" ", passw=" ", conpassw=" "):
+    def registerUser(self, name=" ",isStaff=False, user=" ", passw=" ", conpassw=" "):
         if passw != conpassw:
-            return open(os.path.abspath(__file__)[:-11]+"\clientRegistrationPFail.html")
-        elif loginDatabase.CheckUsername(user) != []:
-            return open(os.path.abspath(__file__)[:-11]+"\clientRegistrationUFail.html")
+            return open(os.getcwd()+r"\Website\Registration\clientRegistrationPFail.html")
+        elif lDB.CheckUsername(user) != [] and user != " ":
+            return open(os.getcwd()+r"\Website\Registration\clientRegistrationUFail.html")
         else:
             lDB.insertNewUser(user, passw)
-            return open(os.path.abspath(__file__)[:-11]+"\clientRegistration.html")
+            uDB.addUser(name, 0, isStaff, 0, 0)
+            return open(os.getcwd()+r"\Website\Registration\clientRegistration.html")
 
 
 if __name__ == '__main__':
